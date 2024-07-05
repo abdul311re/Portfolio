7f7859458cb0151f19d9a0de08119547eb55e664
ABDUL REHMAN <abdul.reh311@gmail.com>
2024-07-06 03:45:06 +0500

    add

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavDropdown ,Nav ,Navbar, Offcanvas,Container, Button , Image} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.scss";
+
+function Header() {
+  const ArLogo = "./Assets/ArLogo.png";
+  return (
+    <>
+    <Container className="navbar-center">
+      {[ 'xl'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid >
+            <Navbar.Brand href="#" className='me-auto Arlogo-nav '><Image src={ArLogo} width={'100%'}/></Navbar.Brand>
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  AR
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-center flex-grow-1 pe-3 gap-3">
+                  <Nav.Link href="#action1" className="fw-bold nav-lists">Home</Nav.Link>
+                  <NavDropdown
+                  className="fw-bold nav-lists"
+                    title="Services"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Web Development</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action4">
+                      Wordpress Development
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Shopify Development
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#action2" className="fw-bold nav-lists">Portfolio</Nav.Link>
+                  <Nav.Link href="#action2" className="fw-bold nav-lists">Blog</Nav.Link>
+                  <Nav.Link href="#action2" className="fw-bold nav-lists">About Us</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+            <Button variant="dark">Contact US</Button>
+            <Navbar.Toggle className="navbartoggle" aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          </Container>
+        </Navbar>
+      ))}
+      </Container>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/Pages/Hero/Heropage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Component , Row  } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Heropage(){
+  return(<>
+     <Container>
+       <Row>
+        <Col></Col>
+        <Col></Col>
+       </Row>
+     </Container>
+  </>)
+}
+export default Heropage;

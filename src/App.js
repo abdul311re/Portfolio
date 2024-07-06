fbdc9c30e743a39978c838c8184c76bbd2e45ed6
ABDUL REHMAN <abdul.reh311@gmail.com>
2024-07-06 05:42:04 +0500

    Mission

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import Heropage from "./Pages/Hero/Heropage";
 import Header from "./Components/Header/Header";
 import './App.css';
+import Mission from "./Components/Mission/Mission";
 
 function App() {
-  return (
+  return (<>
     <div className="App">
       <Header/>
+      <Heropage/>
+      <Mission />
     </div>
-  );
+  </>);
 }
 
 export default App;
--- src/Pages/Hero/Heropage.jsx
+++ src/Pages/Hero/Heropage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Component , Row  } from "react";
+import { Container, Row, Col, Image  } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Heropage.scss";
 function Heropage(){
+  const Mainpage ="./Assets/Heropage/Mainpage.png";
   return(<>
-     <Container>
-       <Row>
-        <Col></Col>
-        <Col></Col>
+     <Container fluid>
+       <Row >
+        <Col sm={6}>
+        <div className="Hero1-dev">
+          <h1 className=" Hero1-head">WEB & WORDPRESS DEVELOPMENT</h1>
+          <p className="py-3">Transform your online space with our expertise in web development and WordPress. We create websites that are as captivating as they are effective, seamlessly blending design excellence with user-friendly functionality. Let us help you tell your brand story and achieve your online goals. </p>
+        </div>
+        </Col>
+        <Col sm={6} ><Image src={Mainpage} width={'100%'}/>
+        </Col>
        </Row>
      </Container>
   </>)
